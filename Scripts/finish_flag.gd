e686fb0b628a4092b377bb7ba28509dc10e75660
Adrian Schofield <adrianschofield@hotmail.co.uk>
2023-12-21 15:06:16 +0000

    Added an animation to the finish flag

--- Scripts/finish_flag.gd
+++ Scripts/finish_flag.gd
@@ -2,6 +2,11 @@ extends Area2D
 
 # @export_file("*.tscn") var next_scene
 
+func _ready():
+	# There doesn't seem to be any way to start an animation automatically
+	# so let's play it whenever the flag is instantiated
+	get_node("AnimatedSprite2D").play()
+
 func _on_body_entered(body):
 	if body.is_in_group("player"):
 		print("end_of_level emit")
